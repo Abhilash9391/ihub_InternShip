057f45a90f1b1bcdc2dc915c1c7f05af90bbb6c0
abhilash <abhi9391reddy@gmail.com>
2025-10-27 21:45:57 +0530

    detection and segmentation on multiple images

---
+++ multidetection.py
@@ -0,0 +1,18 @@
+import glob 
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")
+
+extensions = ['jpg','svg','jpeg']
+
+image_paths = []
+
+for ext in extensions:
+    image_paths.extend(glob.glob(f"images/*.{ext}"))
+
+for path in image_paths:
+    results = model(path,save=True,show=True)
+    
+
+
+
